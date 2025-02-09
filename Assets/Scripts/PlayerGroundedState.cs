d95219230b3ead80be1380a3cd8e793bbc330941
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-09 18:11:00 +0700

    Add player state classes for air, jump, and grounded states with animations.

---
+++ Assets/Scripts/PlayerGroundedState.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerGroundedState : PlayerState
+{
+    public PlayerGroundedState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) 
+    : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            stateMachine.ChangeState(player.jumpState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+    // Sử dụng GroundedState để chuyển sang các trạng thái khác mà ko cần viết lại code lặp trong hàm Update.
+}
