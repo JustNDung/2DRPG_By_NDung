c3bd7c0334407a2ab977bdacf4d821aaf12df796
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-11 22:14:47 +0700

    Add new tile palette assets and their metadata.

--- Assets/Scripts/Player/PlayerWallJumpState.cs
+++ Assets/Scripts/Player/PlayerWallJumpState.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerWallJumpState : PlayerState
 {
+    private float timer = .4f;
+    private float wallJumpForce = 5f;
     public PlayerWallJumpState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) 
     : base(_player, _stateMachine, _animBoolName)
     {
@@ -10,8 +12,8 @@ public class PlayerWallJumpState : PlayerState
     public override void Enter()
     {
         base.Enter();
-        stateTimer = .4f;
-        player.SetVelocity(5 * -player.facingDirection, player.jumpForce);
+        stateTimer = timer;
+        player.SetVelocity(wallJumpForce * -player.facingDirection, player.jumpForce);
     }
 
     public override void Update()
