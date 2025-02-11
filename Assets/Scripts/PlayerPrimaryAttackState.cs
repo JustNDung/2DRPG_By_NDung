2b7e359877f6d9f95b6c927c62dbb19562d700bc
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-11 11:36:40 +0700

    Add primary attack state implementation and integrate with player state machine.

---
+++ Assets/Scripts/PlayerPrimaryAttackState.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerPrimaryAttackState : PlayerState
+{
+    public PlayerPrimaryAttackState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) 
+    : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+    public override void Update()
+    {   
+        base.Update();
+
+        if (triggeredCalled) {
+            stateMachine.ChangeState(player.idleState);
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+}
