dda0c7bf8d5c024f58c39e521f3206b2a2fa9d9c
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-12 16:58:25 +0700

    Refactor ParallaxBackground script: move to new folder and add endless background mechanism.

--- Assets/Scripts/Background/ParallaxBackground.cs
+++ Assets/Scripts/Background/ParallaxBackground.cs
@@ -5,10 +5,13 @@ public class ParallaxBackground : MonoBehaviour
     private GameObject cam;
     [SerializeField] private float parallaxEffect;
     private float xPosition;
+    private float length;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = GameObject.Find("Main Camera");
+
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
         xPosition = transform.position.x;
     }
 
@@ -16,7 +19,17 @@ public class ParallaxBackground : MonoBehaviour
     void Update()
     {
         // Hiệu ứng parallax tạo chiều sâu cho game 2D.
+        float distanceMoved = cam.transform.position.x * (1 - parallaxEffect);
         float distanceToMove = cam.transform.position.x * parallaxEffect;
-        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y);  
+        transform.position = new Vector3(xPosition + distanceToMove, transform.position.y); 
+
+        if (distanceMoved > xPosition + length)
+        {
+            xPosition += length;
+        }
+        else if (distanceMoved < xPosition - length)
+        {
+            xPosition -= length;
+        }
     }
 }
