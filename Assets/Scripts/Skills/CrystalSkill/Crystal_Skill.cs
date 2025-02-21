9190d4913d12c1cf60a648c0295e5a5d86300f55
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-21 14:54:29 +0700

    Implement explosive and moving crystal mechanics with animation events and collider setup.

--- Assets/Scripts/Skills/CrystalSkill/Crystal_Skill.cs
+++ Assets/Scripts/Skills/CrystalSkill/Crystal_Skill.cs
@@ -6,6 +6,13 @@ public class Crystal_Skill : Skill
     [SerializeField] private GameObject crystalPrefab;
     private GameObject currentCrystal;
 
+    [Header("Explosive crystal")] 
+    [SerializeField] private bool canExplode;
+
+    [Header("Moving crystal")] 
+    [SerializeField] private bool canMoveToEnemy;
+    [SerializeField] private float moveSpeed;
+    
     public override void UseSkill()
     {
         base.UseSkill();
@@ -14,12 +21,15 @@ public class Crystal_Skill : Skill
         {
             currentCrystal = Instantiate(crystalPrefab, player.transform.position, Quaternion.identity);
             Crystal_Skill_Controller crystalSkillController = currentCrystal.GetComponent<Crystal_Skill_Controller>();
-            crystalSkillController.SetupCrystal(crystalDuration);
+            crystalSkillController.SetupCrystal(crystalDuration, canExplode, canMoveToEnemy, moveSpeed);
         }
         else
         {
+            Vector2 playerPos = player.transform.position;
+            
             player.transform.position = currentCrystal.transform.position;
-            Destroy(currentCrystal);
+            currentCrystal.transform.position = playerPos;
+            currentCrystal.GetComponent<Crystal_Skill_Controller>()?.FinishCrystal();
         }
     }
 }
