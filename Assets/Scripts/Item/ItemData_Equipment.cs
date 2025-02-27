41438c0f4b978f676cb42f8fa2b57b538ec5b6dc
JustNDung <nguyenngocdung19122005@gmail.com>
2025-02-27 21:07:08 +0700

    Add equipment modifiers to ItemData_Equipment and update Inventory management.

--- Assets/Scripts/Item/ItemData_Equipment.cs
+++ Assets/Scripts/Item/ItemData_Equipment.cs
@@ -11,4 +11,66 @@ public enum EquipmentType
 public class ItemData_Equipment : ItemData
 {
     public EquipmentType equipmentType;
+
+    [Header("Major stats")] public float strength;
+    public float agility;
+    public float intelligence;
+    public float vitality;
+
+    [Header("Offensive stats")] public float damage;
+    public float critChance;
+    public float critPower;
+
+    [Header("Defensive stats")] public float health;
+    public float armor;
+    public float evasion;
+    public float magicResistance;
+
+    [Header("Magic stats")] public float fireDamage;
+    public float iceDamage;
+    public float lightDamage;
+
+    public void AddModifiers()
+    {
+        PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        playerStats.strength.AddModifier(strength);
+        playerStats.agility.AddModifier(agility);
+        playerStats.intelligence.AddModifier(intelligence);
+        playerStats.vitality.AddModifier(vitality);
+
+        playerStats.damage.AddModifier(damage);
+        playerStats.critChance.AddModifier(critChance);
+        playerStats.critPower.AddModifier(critPower);
+
+        playerStats.maxHp.AddModifier(health);
+        playerStats.armor.AddModifier(armor);
+        playerStats.evasion.AddModifier(evasion);
+        playerStats.magicResistance.AddModifier(magicResistance);
+
+        playerStats.fireDamage.AddModifier(fireDamage);
+        playerStats.lightningDamage.AddModifier(lightDamage);
+    }
+
+    public void RemoveModifiers()
+    {
+        PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        playerStats.strength.RemoveModifier(strength);
+        playerStats.agility.RemoveModifier(agility);
+        playerStats.intelligence.RemoveModifier(intelligence);
+        playerStats.vitality.RemoveModifier(vitality);
+
+        playerStats.damage.RemoveModifier(damage);
+        playerStats.critChance.RemoveModifier(critChance);
+        playerStats.critPower.RemoveModifier(critPower);
+
+        playerStats.maxHp.RemoveModifier(health);
+        playerStats.armor.RemoveModifier(armor);
+        playerStats.evasion.RemoveModifier(evasion);
+        playerStats.magicResistance.RemoveModifier(magicResistance);
+
+        playerStats.fireDamage.AddModifier(fireDamage);
+        playerStats.lightningDamage.RemoveModifier(lightDamage);
+    }
 }
